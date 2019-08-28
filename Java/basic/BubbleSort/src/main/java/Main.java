be9cab205af012b9d2a158d97e619e47bb16ddd7
kmluns <kamil.unsal@icterra.com>
2019-08-28 10:18:53 +0300

    added bubble sort

---
+++ Java/basic/BubbleSort/src/main/java/Main.java
@@ -0,0 +1,25 @@
+import bubblesort.BubbleSort;
+
+/**
+ * created by kmluns 28.08.2019
+ **/
+public class Main {
+    public static void main(String args[]){
+        int[] arr = new int[]{5,3,1,2,21,0};
+
+        System.out.println("Initial Array");
+        printArray(arr);
+
+        BubbleSort.sort(arr);
+
+        System.out.println("Sorted Array");
+        printArray(arr);
+    }
+
+    private static void printArray(int[] arr){
+        for (int j = 0; j < arr.length; j++) {
+            System.out.print("arr["+ j + "] : " + arr[j] + ", ");
+        }
+        System.out.println();
+    }
+}
